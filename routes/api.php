d7c4eb95c9df8bad387f0d31136e8b64f8b9a413
mohammad <mohammad@inpinapp.com>
2019-02-16 15:17:58 +0330

    Init commit.

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['as' => 'api.', 'namespace' => 'Api'], function () {
+    Route::group(['prefix' => 'company', 'as' => 'company.', 'namespace' => 'Company'], function () {
+        Route::apiResource('', 'CompanyController', ['parameters' => ['' => 'company']]);
+        Route::group(['prefix' => '{company}'], function () {
+            Route::apiResource('station', 'StationController', ['only' => 'index']);
+            Route::apiResource('sub-companies', 'SubCompanyController', ['only' => 'index', 'parameters' => ['sub-companies' => 'subCompanies']]);
+        });
+    });
+
+    Route::group(['prefix' => 'station', 'as' => 'station.', 'namespace' => 'Station'], function () {
+        Route::apiResource('', 'StationController', ['parameters' => ['' => 'station']]);
+        Route::get('index-within-radius/{latitude}/{longitude}/{radius?}', 'StationController@indexWithinRadius')->name('index-within-radius');
+    });
+});
