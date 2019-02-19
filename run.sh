24c6e50a2351e1e6c5709edac1f8e7e94dcac434
mohammad <mohammad@inpinapp.com>
2019-02-20 01:11:06 +0330

    Remove setup commands from run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Create valid .env file
-cp -rf .env.docker .env
-
 cd laradock
 
 # Create valid .env file for laradock
@@ -10,14 +7,5 @@ cp -rf .env.docker .env
 
 # Run containers
 docker-compose up -d nginx mariadb phpmyadmin redis workspace elasticsearch
-
-# Run composer install and migrate the database
-docker-compose exec workspace bash -c "composer install"
-docker-compose exec workspace bash -c "php artisan optimize"
-docker-compose exec workspace bash -c "php artisan migrate"
-
-# Install npm and prepare for production
-docker-compose exec workspace bash -c "npm install"
-docker-compose exec workspace bash -c "npm run prod"
 
 cd ..
